f706bc98214cb3e44043969bcf7b3b3395945722
a7medelbahy <ahmedelbahy06@gmail.com>
2023-02-15 14:37:34 +0200

    T-Movies

--- src/App.jsx
+++ src/App.jsx
@@ -5,52 +5,52 @@ import Movies from "./pages/movies/Movies";
 import Series from "./pages/series/Series";
 import Footer from "./components/Footer";
 import Details from "./pages/details/Details";
-import { useState,useEffect } from "react";
-import { FidgetSpinner } from  'react-loader-spinner'
+import { useState, useEffect } from "react";
+import { FidgetSpinner } from "react-loader-spinner";
 import NotFound from "./pages/notFound/NotFound";
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  useEffect(()=>{
+  useEffect(() => {
     setLoading(true);
     setTimeout(() => {
-      setLoading(false)
+      setLoading(false);
     }, 3000);
-  },[])
-  const styling={
-    position:'absolute',
-    top:"50%",
-    left:"50%",
-    transform:"translate(-50%,-50%)"
-  }
-  
+  }, []);
+  const styling = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+  };
+
   return (
     <>
-    {loading?<FidgetSpinner
-  visible={loading}
-  height="100"
-  width="100"
-  ariaLabel="dna-loading"
-  wrapperStyle={styling}
-  wrapperClass="dna-wrapper"
-  ballColors={['#ff0000', '#1aca40', '#0000ff']}
-  backgroundColor="#FFF"
-/>
-   :
-
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" index element={<Home />} />
-          <Route path="/T-Movies" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/series" element={<Series />} />
-          <Route path="/details/:id/:type" element={<Details />} />
-          <Route path='/*' element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-  }
+      {loading ? (
+        <FidgetSpinner
+          visible={loading}
+          height="100"
+          width="100"
+          ariaLabel="dna-loading"
+          wrapperStyle={styling}
+          wrapperClass="dna-wrapper"
+          ballColors={["#ff0000", "#1aca40", "#0000ff"]}
+          backgroundColor="#FFF"
+        />
+      ) : (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" index element={<Home />} />
+            <Route path="/T-Movies" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/series" element={<Series />} />
+            <Route path="/details/:id/:type" element={<Details />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      )}
     </>
   );
 };
